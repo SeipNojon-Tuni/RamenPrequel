3a9599b98e7ab93dfc60ad03e8e553857ead0a0f
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2021-04-08 19:00:41 +0300

    Added video to start dialogue

--- RamenPrequel/Dialog-System-Example/scripts/dialog_player.gd
+++ RamenPrequel/Dialog-System-Example/scripts/dialog_player.gd
@@ -8,6 +8,11 @@ onready var _SpaceBar_Icon = self.find_node("SpaceBar_NinePatchRect")
 
 onready var _audio_player = get_tree().get_current_scene().get_node("AudioPlayer")
 
+onready var _video = get_parent().get_node("Video/VideoPlayer")
+
+# Background animation, only in story start sequence
+onready var _bg_anim = get_parent().get_node("IntroAnimation")
+
 var _did = 0
 var _nid = 0
 var _final_nid = 0
@@ -100,3 +105,8 @@ func _play_node():
 	if(_audio_player):
 		# Play current audio track
 		_audio_player.play_next_dialogue()
+	
+	# Change to video with second dialogue node
+	if(_nid == 3 && _bg_anim && _video):
+		_bg_anim.queue_free()
+		_video.play()
