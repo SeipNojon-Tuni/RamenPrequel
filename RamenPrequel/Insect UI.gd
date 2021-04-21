b6a3278cb91ef7117198f8a427d1be893f83ff19
Lasinen <78957032+Lasinen@users.noreply.github.com>
2021-04-21 16:20:50 +0300

    Edited insect scene
    Insect scene scoring system fixed
    Placed clutter for visual obstacles
    Fixed score text counter

--- RamenPrequel/Insect UI.gd
+++ RamenPrequel/Insect UI.gd
@@ -2,10 +2,53 @@ extends Control
 
 onready var scoreText = get_node("ScoreText")
 
-func set_score_text (score):
+var score = 0
+var value = 1
 
-	scoreText.text = str("Blight bugs collected: ",score,"/8")
 
-func _ready ():
- 
-	scoreText.text = "Blight bugs collected: 0/8"
+func set_score_text ():
+	scoreText.text = str("Blight bugs collected: ",score,"/11")
+	
+func collect_insect():
+		score += value
+		set_score_text()
+		if win():
+			get_tree().change_scene("res://Scenes/MainMenu.tscn")
+			queue_free()
+	
+func win ():
+	if score == 11:
+		get_tree().change_scene("res://Scenes/MainMenu.tscn")
+	
+
+func _on_Insect_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			collect_insect()
+
+
+
+
+func _on_Insect5_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			collect_insect()
+
+
+
+func _on_Insect2_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			collect_insect()
+
+
+func _on_Insect3_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			collect_insect()
+
+
+func _on_Insect4_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			collect_insect()
--- RamenPrequel/InsectClicked.gd
+++ RamenPrequel/InsectClicked.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
 onready var sprite = $Sprite
-onready var ui = get_node("res://Insect UI.gd")
-export var value = 1
-var score : int = 0
 
 func _ready():
 	pass # Replace with function body.
@@ -16,14 +13,40 @@ func _on_Insect_mouse_exited():
 	sprite.modulate = Color(1, 1, 1)
 
 
-func _on_Insect_input_event(viewport, event, shape_idx):
+func _on_Insect_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			queue_free()
-		if win():
-			print("Win")
-			queue_free()
+			remove_child($Sprite)
+			remove_child($CollisionPolygon2D)
 
-func win():
-	score == 8
 
+func _on_Insect5_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			remove_child($Sprite)
+			remove_child($CollisionPolygon2D)
+
+
+func _on_Insect2_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			remove_child($Sprite)
+			remove_child($CollisionPolygon2D)
+
+
+func _on_Insect3_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			remove_child($Sprite)
+			remove_child($CollisionPolygon2D)
+
+
+func _on_Insect4_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			remove_child($Sprite)
+			remove_child($CollisionPolygon2D)
+
+
+func _on_Insect5_mouse_exited():
+	sprite.modulate = Color(1, 1, 1)
